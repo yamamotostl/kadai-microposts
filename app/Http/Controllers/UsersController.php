6cb5ddc92b87962631a79ce350280d7833254883
EC2 Default User <ec2-user@ip-172-31-80-124.ec2.internal>
2019-06-20 04:52:34 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,4 +15,20 @@ class UsersController extends Controller
             'users' => $users,
         ]);
     }
+    
+    //extendsしたcontrollerで定義したcountsメソッドを使用
+    public function show($id)
+    {
+        $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'microposts' => $microposts,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.show', $data);
+    }
 }
